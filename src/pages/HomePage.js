55fc31cc9965d6fdc0f0544a3f5745cc8505dfd8
Rosela <rberberi22@epoka.edu.al>
2024-10-24 21:29:30 +0200

    minor fixes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,7 @@
-import React, { useReducer, useEffect, useCallback, useState, useMemo } from 'react';
+import React, { useReducer, useEffect,  useMemo } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import AddTask from './AddTask'
-import EditTask from './EditTask'
 import axios from 'axios';
 import ToDoList from '../components/ToDoList';
-import ToDoForm from '../components/ToDoForm';
 
 const API_URL = 'http://localhost:8080/todos';
 
@@ -19,8 +16,6 @@ const initialState = {
 const actionTypes = {
   FETCH_SUCCESS: 'FETCH_SUCCESS',
   FETCH_FAILURE: 'FETCH_FAILURE',
-  ADD_TASK: 'ADD_TASK',
-  UPDATE_TASK: 'UPDATE_TASK',
   DELETE_TASK: 'DELETE_TASK',
   TOGGLE_TASK: 'TOGGLE_TASK',
 };
@@ -30,19 +25,7 @@ const todoReducer = (state, action) => {
     case actionTypes.FETCH_SUCCESS:
       return { ...state, loading: false, tasks: action.payload };
     case actionTypes.FETCH_FAILURE:
-      return { ...state, loading: false, error: action.payload };
-    case actionTypes.ADD_TASK:
-      return { ...state, tasks: [...state.tasks, action.payload] };
-      case actionTypes.UPDATE_TASK:
-        return {
-          ...state,
-          tasks: state.tasks.map((task) =>
-            task.id === action.payload.id 
-              ? { ...task, ...action.payload } 
-              : task
-          ),
-        };
-      
+      return { ...state, loading: false, error: action.payload };  
     case actionTypes.DELETE_TASK:
       return {
         ...state,
@@ -67,25 +50,21 @@ const HomePage=()=>{
   const[searchParams, setSearchParams]=useSearchParams()
   const filter = searchParams.get('filter');
 
- 
-  const [isEditing, setIsEditing] = useState(false);
-  const [currentTaskId, setCurrentTaskId] = useState(null);
 
- 
-  const fetchTasks = useCallback(async () => {
-    try {
-      const response = await axios.get(API_URL);
-      dispatch({ type: actionTypes.FETCH_SUCCESS, payload: response.data });
-      console.log('fetching')
-    } catch (err) {
-      dispatch({ type: actionTypes.FETCH_FAILURE, payload: err.message });
-    }
-  }, []);
 
-  
   useEffect(() => {
+    const fetchTasks =async () => {
+      try {
+        const response = await axios.get(API_URL);
+        dispatch({ type: actionTypes.FETCH_SUCCESS, payload: response.data });
+        console.log('fetching')
+      } catch (err) {
+        dispatch({ type: actionTypes.FETCH_FAILURE, payload: err.message });
+      }
+    };
+
     fetchTasks();
-  }, [fetchTasks]);
+  }, []);
 
   const filteredTasks = useMemo(() => {
     if (filter === 'completed') {
@@ -94,21 +73,9 @@ const HomePage=()=>{
     if (filter === 'uncompleted') {
       return tasks.filter(task => !task.completed);
     }
-    return tasks; // Return all tasks if no filter is set
+    return tasks; 
   }, [tasks, filter]);
 
-
-  // const handleAddTask = async (task) => {
-  //   try {
-  //     const response = await axios.post(API_URL, {
-  //       title: task.title,
-  //       description: task.description,
-  //     });
-  //     dispatch({ type: actionTypes.ADD_TASK, payload: response.data });
-  //   } catch (error) {
-  //     console.error('Error adding task:', error);
-  //   }
-  // };
 
   const handleAddTask = () => {
     navigate('/add-task'); 
@@ -126,19 +93,6 @@ const handleCheckboxChange = async (id, completed) => {
 };
 
 
-// const handleUpdateTask = async (updatedTask) => {
-//   try {
-//     const response = await axios.patch(`${API_URL}/${updatedTask.id}`, {
-//       title: updatedTask.title,
-//       description: updatedTask.description,
-//     });
-
-//     dispatch({ type: actionTypes.UPDATE_TASK, payload: response.data });
-//   } catch (error) {
-//     console.error('Error updating task:', error);
-//   }
-// };
-
 
   const handleDeleteTask = async (id) => {
     try {
@@ -149,19 +103,11 @@ const handleCheckboxChange = async (id, completed) => {
     }
   };
 
-  // const handleEditTask = (id) => {
-  //   setIsEditing(true);
-  //   setCurrentTaskId(id);
-  // };
 
   const handleEditTask = (id) => {
     navigate(`/edit-task/${id}`);
   };
 
-  // const handleCancelEdit = () => {
-  //   setIsEditing(false);
-  //   setCurrentTaskId(null);
-  // };
 
 
   const completedCount = useMemo(() => {
@@ -183,15 +129,7 @@ const handleCheckboxChange = async (id, completed) => {
 
       <p><strong>Completed tasks: </strong>{completedCount}</p>
       <p><strong>Uncompleted tasks:</strong> {uncompletedCount}</p>
-      
 
-
-      {/* <ToDoForm
-        tasks={tasks}             
-        currentTaskId={currentTaskId} 
-        onSubmit={isEditing ? handleUpdateTask : handleAddTask} 
-        onCancel={handleCancelEdit}   
-      /> */}
       <div>
        <button className="add-btn" onClick={handleAddTask}>Add Task</button>
        <select defaultValue=""
